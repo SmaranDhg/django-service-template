8333b568a1b62da697bfa94c9f79b7c84e374131
Smaran Dhungana <smarandhg@gmail.com>
2024-06-12 21:12:26 +0545

    fix(patient:admin): remove the age

--- lib/apps/patient_management/admin.py
+++ lib/apps/patient_management/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from .models import *
 
+
 @admin.register(Patient)
 class PatientAdmin(admin.ModelAdmin):
-    list_display = ('full_name', 'gender', 'phone_number', 'date_of_birth', 'age', 'address')
-    search_fields = ('full_name', 'phone_number')
-    list_filter = ('gender',)
+    list_display = ("full_name", "gender", "phone_number", "date_of_birth", "address")
+    search_fields = ("full_name", "phone_number")
+    list_filter = ("gender",)
